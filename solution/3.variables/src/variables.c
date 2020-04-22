5934af1806cc30f8006163c2ea8d873802b59af4
Rafael Fillipe Silva <silva.rafaelfillipe@gmail.com>
2020-04-22 19:31:31 -0300

    Follow task 3 description for operations.
    Signed-off-by: Rafael Fillipe Silva <silva.rafaelfillipe@gmail.com>

--- solution/3.variables/src/variables.c
+++ solution/3.variables/src/variables.c
@@ -6,9 +6,15 @@ void run_variables()
     int first_num  = 4;
     int second_num = 2;
 
+    int addition    = first_num + second_num;
+    int subtraction = first_num - second_num;
+    int product     = first_num * second_num;
+    int quotient    = first_num / second_num;
+
     printf("Numeros: %d e %d\n", first_num, second_num);
-    printf("Soma: %d\n", first_num + second_num);
-    printf("Diferenca: %d\n", first_num - second_num);
-    printf("Produto: %d\n", first_num * second_num);
-    printf("Quociente: %d\n", first_num / second_num);
+
+    printf("Soma: %d\n", addition);
+    printf("Diferenca: %d\n", subtraction);
+    printf("Produto: %d\n", product);
+    printf("Quociente: %d\n", quotient);
 }
