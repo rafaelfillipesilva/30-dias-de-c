c26fcc94f2fe5cf226725828e047e991c20ae04f
Rafael Fillipe Silva <silva.rafaelfillipe@gmail.com>
2020-04-27 17:25:02 -0300

    Move floating-point number comparison to number_utils helper.

--- src/solution/6.velocidade/tests/velocidade.cpp
+++ src/solution/6.velocidade/tests/velocidade.cpp
@@ -3,22 +3,17 @@
 #include <cmath>
 #include <limits>
 #include <boost/test/unit_test.hpp>
+#include "tests/number_utils.hpp"
 #include "velocidade.h"
 
 BOOST_AUTO_TEST_SUITE(velocidade)
 
 BOOST_AUTO_TEST_CASE(convert_mps_to_kmph)
 {
-    auto cmp_d = [](const double left, const double right)
-    {
-        constexpr auto epsilon = std::numeric_limits<double>::epsilon();
-        return (std::fabs(left - right) <= epsilon);
-    };
-
-    BOOST_CHECK(cmp_d(mps_to_kmph(+10.0), +36.0));
-    BOOST_CHECK(cmp_d(mps_to_kmph(-10.0), -36.0));
-    BOOST_CHECK(cmp_d(mps_to_kmph(32.00), 115.2));
-    BOOST_CHECK(cmp_d(mps_to_kmph(-0.00), +0.00));
+    BOOST_CHECK(epsilon_cmp(mps_to_kmph(+10.0), +36.0));
+    BOOST_CHECK(epsilon_cmp(mps_to_kmph(-10.0), -36.0));
+    BOOST_CHECK(epsilon_cmp(mps_to_kmph(32.00), 115.2));
+    BOOST_CHECK(epsilon_cmp(mps_to_kmph(-0.00), +0.00));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
